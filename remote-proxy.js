21b2f281f89d332c0866abfed76902259ea980b4
Dhruv <dhruvbird@gmail.com>
2012-11-30 10:58:22 -0500

    Reformat and remove comment.

--- remote-proxy.js
+++ remote-proxy.js
@@ -28,37 +28,37 @@ var fs    = require('fs');
 var dns   = require('dns');
 
 var TIMEOUT_SEC = 80;
-
+var HTTP_LISTEN_PORT = 9769;
 
 function map_hash(m, mapper) {
-	var r = { };
-	for (var k in m) {
-		r[k] = mapper(k, m[k]);
-	}
-	return r;
+    var r = { };
+    for (var k in m) {
+	r[k] = mapper(k, m[k]);
+    }
+    return r;
 }
 
 function hitch(obj, proc) {
-	return function() {
-		return proc.apply(obj, arguments);
-	};
+    return function() {
+	return proc.apply(obj, arguments);
+    };
 }
 
 /* Try to not catch unhandled exceptions in the hope of tracking line numbers */
 process.on('uncaughtException', function (err) {
-	console.log('(un)Caught exception: ', err.stack);
+    console.log('(un)Caught exception: ', err.stack);
 });
 
 
 var https_options = {
-	key: fs.readFileSync('./openSSL_keys/ryans-key.pem'), 
-	cert: fs.readFileSync('./openSSL_keys/ryans-cert.pem')
+    key: fs.readFileSync('./openSSL_keys/ryans-key.pem'), 
+    cert: fs.readFileSync('./openSSL_keys/ryans-cert.pem')
 };
 
 var opts = require('tav').set();
 
 if (opts.timeout) {
-	TIMEOUT_SEC = parseInt(opts.timeout < 60 ? 60 : opts.timeout);
+    TIMEOUT_SEC = parseInt(opts.timeout < 60 ? 60 : opts.timeout);
 }
 
 
@@ -66,133 +66,127 @@ var np_req = 0;
 
 
 https.createServer(https_options, function (req, res) {
-	// console.log(req);
-	// console.log("Request Headers:", req.headers);
+    // console.log(req);
+    // console.log("Request Headers:", req.headers);
 
-	var headers = req.headers;
-	var u       = url.parse(req.url);
-	var host    = headers['host'];
-	var port    = u.port || 80;
-	var search  = u.search || '';
-	var _terminated = false;
+    var headers = req.headers;
+    var u       = url.parse(req.url);
+    var host    = headers['host'];
+    var port    = u.port || 80;
+    var search  = u.search || '';
+    var _terminated = false;
 
-	++np_req;
+    ++np_req;
 
-	console.log(np_req, "Requesting URL:", host + req.url);
+    console.log(np_req, "Requesting URL:", host + req.url);
 
 
-	// console.log("host:", host);
-	// console.log("url:", u);
+    // console.log("host:", host);
+    // console.log("url:", u);
 
-	// Create an timeout object to timeout our connection if there is
-	// no data transfer happening for TIMEOUT_SEC second.
-	var to_interval = null;
+    // Create an timeout object to timeout our connection if there is
+    // no data transfer happening for TIMEOUT_SEC second.
+    var to_interval = null;
 
-	/* Reset the timeout so that we start counting once again after
-	 * any activity has occurred.
-	 */
-	function reset_timeout() {
-		// console.log("Resetting timeout for URL:", host + req.url);
-		unset_timeout();
+    /* Reset the timeout so that we start counting once again after
+     * any activity has occurred.
+     */
+    function reset_timeout() {
+	// console.log("Resetting timeout for URL:", host + req.url);
+	unset_timeout();
 
-		to_interval = setTimeout(function() {
-			console.error("Timing out request:", host + req.url);
-			rreq.destroy();
-		}, TIMEOUT_SEC * 1000);
+	to_interval = setTimeout(function() {
+	    console.error("Timing out request:", host + req.url);
+	    rreq.destroy();
+	}, TIMEOUT_SEC * 1000);
+    }
+
+    function unset_timeout() {
+	// console.log("clearing Timeout for:", host + req.url);
+	if (to_interval) {
+	    clearTimeout(to_interval);
+	    to_interval = null;
 	}
+    }
 
-	function unset_timeout() {
-		// console.log("clearing Timeout for:", host + req.url);
-		if (to_interval) {
-			clearTimeout(to_interval);
-			to_interval = null;
-		}
+    reset_timeout();
+
+
+    function terminate_request(streams) {
+	if (!_terminated) {
+	    --np_req;
+	    console.error(np_req, "Hard terminating request:", req.url);
+	    _terminated = true;
+	    unset_timeout();
+
+	    streams.forEach(function(stream) {
+		stream.destroy();
+	    });
 	}
+    }
 
+    // The remote request object
+    var rreq = http.request({
+	host: host, 
+	port: port, 
+	path: u.pathname + search, 
+	method: req.method, 
+	agent: false
+    }, function (rres) {
+	// console.log("rres:", rres);
+	var rheaders = rres.headers;
+
+	// Remove the "Content-Encoding" header.
+	// if (rheaders['content-encoding']) {
+	// delete rheaders['content-encoding'];
+	// }
+
+	res.writeHead(rres.statusCode, rheaders);
+
+	// Pipe all data from source (rres) to destination (res)
+	// rres.pipe(res);
+
+	rres.on('data', function(d) {
+	    res.write(d);
+	    reset_timeout();
+	}).on('end', function() {
+	    unset_timeout();
+	    --np_req;
+	    console.log(np_req, "Received Complete Response for URL:", host + req.url);
+	    res.end();
+	});
+
+	rres.on('error', function() {
+	    console.log("Error getting HTTP response:", arguments);
+	    // Don't forget to destroy the server's response stream
+	    terminate_request([res]);
+	});
+    });
+
+    // console.log("RREQ:", rreq);
+
+    rreq.on('error', function() {
+	console.error("Error connecting:", arguments);
+	terminate_request([res]);
+    });
+
+    // Write out the headers.
+    map_hash(headers, hitch(rreq, rreq.setHeader));
+
+    // Pipe the result from the local proxy (req) to the actual host (rreq)
+    // req.pipe(rreq);
+    req.on('data', function(d) {
+	rreq.write(d);
 	reset_timeout();
+    }).on('end', function() {
+	rreq.end();
+	reset_timeout();
+    });
 
+    // Destroy the stream on error
+    req.on('error', function() {
+	console.log("Error sending data to client:", arguments);
+	terminate_request([rreq]);
+    });
 
-	function terminate_request(streams) {
-		if (!_terminated) {
-			--np_req;
-			console.error(np_req, "Hard terminating request:", req.url);
-			_terminated = true;
-			unset_timeout();
-
-			streams.forEach(function(stream) {
-				stream.destroy();
-			});
-		}
-	}
-
-	// The remote request object
-	var rreq = http.request({
-	    host: host, 
-	    port: port, 
-	    path: u.pathname + search, 
-	    method: req.method, 
-	    agent: false
-	}, function (rres) {
-		// console.log("rres:", rres);
-		var rheaders = rres.headers;
-
-		// Remove the "Content-Encoding" header.
-		// if (rheaders['content-encoding']) {
-			// delete rheaders['content-encoding'];
-		// }
-
-		res.writeHead(rres.statusCode, rheaders);
-
-		// Pipe all data from source (rres) to destination (res)
-		// rres.pipe(res);
-
-		rres.on('data', function(d) {
-			res.write(d);
-			reset_timeout();
-		})
-		.on('end', function() {
-			unset_timeout();
-			--np_req;
-			console.log(np_req, "Received Complete Response for URL:", host + req.url);
-			res.end();
-		});
-
-		rres.on('error', function() {
-			console.log("Error getting HTTP response:", arguments);
-			// Don't forget to destroy the server's response stream
-			terminate_request([res]);
-		});
-	});
-
-	// console.log("RREQ:", rreq);
-
-	rreq.on('error', function() {
-		console.error("Error connecting:", arguments);
-		terminate_request([res]);
-	});
-
-	// Write out the headers.
-	// Set the "Accept-Encoding" header.
-	// headers['accept-encoding'] = 'gzip,deflate';
-	map_hash(headers, hitch(rreq, rreq.setHeader));
-
-	// Pipe the result from the local proxy (req) to the actual host (rreq)
-	// req.pipe(rreq);
-	req.on('data', function(d) {
-		rreq.write(d);
-		reset_timeout();
-	})
-	.on('end', function() {
-		rreq.end();
-		reset_timeout();
-	});
-
-	// Destroy the stream on error
-	req.on('error', function() {
-		console.log("Error sending data to client:", arguments);
-		terminate_request([rreq]);
-	});
-
-}).listen(9769);
-
-
+}).listen(HTTP_LISTEN_PORT);
